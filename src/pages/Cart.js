ccd21d55eb3365cdaafc90dee73c94f89d2cce0c
shweta yadav <shwetaa.yadav11@gamil.com>
2026-03-19 03:53:03 +0530

    Fix Cart.js unused import

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { apiPlaceOrder } from '../api';
 
 function Cart({ cartItems, setCartItems, setCurrentPage, loggedInUser }) {
